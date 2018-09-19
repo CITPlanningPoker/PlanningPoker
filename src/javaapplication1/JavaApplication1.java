f06cc228343d29e690dd2e4ff749c331fea8dea7
joshuatyra <joshuatyra@G214-01.campus.bpcc.edu>
2018-09-19 11:40:41 -0500

    "[finishes #160561254] Test"

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -19,6 +19,7 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("face");
+        System.out.print("test2");
     }
     
 }
