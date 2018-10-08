c57ee953e999e0fa4ce462e8fc1105950c896c7a
God-Man <godman0912@gmail.com>
2018-10-07 19:53:24 -0500

    Created Contact Us page template, added new image, modifed controller for new page

---
+++ application/views/Contact.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<?php echo link_tag('assets/css/buttonStyle.css'); ?>
+<?php echo link_tag('assets/css/homepageStyle.css'); ?>
+<?php echo link_tag('assets/css/loginStyle.css'); ?>
+<?php echo link_tag('assets/css/navBar.css'); ?>
+<html>
+<head>
+	<title>Planning Poker Home</title>
+</head>
+<body class="bg" background="<?php echo base_url();?>/assets/images/contact_us.png">
+</body>
+</html>
